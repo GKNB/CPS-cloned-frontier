dd719231e1b3b3a7ebfd7127fa0270044e21f6ce
ckelly <ckelly@fb734a29-28db-4bc5-88a3-3cb5ee595545>
2013-02-19 22:52:30 +0000

    -Added ability to write to CorrelationFunction in a multi-threaded environment
    git-svn-id: file:///home/chulwoo/CPS/repo/CVS2SVN/cps/branches/v5_0_19_ckelly_gparity@4495 fb734a29-28db-4bc5-88a3-3cb5ee595545

--- cps_pp/include/alg/correlationfunction.h
+++ cps_pp/include/alg/correlationfunction.h
@@ -10,18 +10,28 @@ CPS_START_NAMESPACE
 
 
 class CorrelationFunction{
+public:
+  enum ThreadType { THREADED, UNTHREADED };
+private:
   char *label;
   int ncontract;
   int time_size;
-  Rcomplex** wick; //the wick contractions, each a function of t
+  Rcomplex** wick; //unthreaded wick contractions [contraction][t]
+  Rcomplex*** wick_threaded; //threaded wick contractions [thread][contraction][t]
 
-  void sumLattice(); //sum each element of wick[contraction][t] over all nodes
+  ThreadType threadtype;
+  int max_threads; //the max amount of threads when the array of Rcomplex was created
 public:
   void setNcontractions(const int &n);
   void write(const char *file);
   void write(FILE *fp);
   Rcomplex & operator()(const int &contraction_idx, const int &t);
-  CorrelationFunction(const char *_label);
+  Rcomplex & operator()(const int &thread_idx, const int &contraction_idx, const int &t);
+
+  void sumLattice(); //sum each element of wick[contraction][t] over all nodes (and threads if threaded). Result is stored in wick and after sum accessible via operator() (non-threaded version)
+  void clear();
+
+  CorrelationFunction(const char *_label, const ThreadType &thread = UNTHREADED);
   ~CorrelationFunction();
 };
 
