490a3635b385582b809171f2795e5c418e6f0181
Chulwoo Jung <chulwoo@bnl.gov>
2017-09-11 21:48:19 -0400

    Checking in to a new branch for debugging

--- cps_pp/include/util/lattice/fgrid.h
+++ cps_pp/include/util/lattice/fgrid.h
@@ -40,7 +40,8 @@ namespace Grid
       for (int i = 0; i < neig; i++) {
 	Grid::ComplexD coef = innerProduct (evec[i], in);
 	coef = coef / eval[i];
-//          std::cout<<GridLogMessage <<"eval coef norm(evec) "<<i<<" : "<<eval[i]<<" "<<coef<<" "<<norm2(evec[i])<< std::endl;
+//	if (cps::VRB.IsActivated(cps::VERBOSE_DEBUG_LEVEL))
+          std::cout<<GridLogMessage <<"eval coef norm(evec) "<<i<<" : "<<eval[i]<<" "<<coef<<" "<<norm2(evec[i])<< std::endl;
 	out += coef * evec[i];
       }
       std::
