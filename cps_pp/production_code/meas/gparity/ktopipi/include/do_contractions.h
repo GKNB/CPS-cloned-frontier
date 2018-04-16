c46e40842302e95745d12384b1d5585490b32660
Christopher Kelly <ckelly@phys.columbia.edu>
2018-04-16 15:40:31 -0400

    Added new K->pipi main program options USE_SYMMETRIC_PION_MOM_POLICIES and USE_STANDARD_AND_SYMMETRIC_PION_MOM_POLICIES for performing calculations with just the pion momenta symmetrized

--- cps_pp/production_code/meas/gparity/ktopipi/include/do_contractions.h
+++ cps_pp/production_code/meas/gparity/ktopipi/include/do_contractions.h
@@ -10,6 +10,11 @@ void doContractionsBasic(const int conf, Parameters &params, const CommandLineAr
   typedef StationarySigmaMomentaPolicy SigmaMomentumPolicy; //as we just save the meson fields this is unimportant (only applies to sigma because we do +/-p anyway)
   typedef SymmetricPionMomentaPolicy PionMomentumPolicy;
   typedef SymmetricLSWWmomentaPolicy LSWWmomentumPolicy;
+#elif defined(USE_SYMMETRIC_PION_MOM_POLICIES) //pion only symmetrized
+  typedef StationaryKaonMomentaPolicy KaonMomentumPolicy;
+  typedef StationarySigmaMomentaPolicy SigmaMomentumPolicy;
+  typedef SymmetricPionMomentaPolicy PionMomentumPolicy;
+  typedef StandardLSWWmomentaPolicy LSWWmomentumPolicy;
 #else
   typedef StationaryKaonMomentaPolicy KaonMomentumPolicy;
   typedef StationarySigmaMomentaPolicy SigmaMomentumPolicy;
@@ -143,6 +148,87 @@ void doContractionsStandardAndSymmetric(const int conf, Parameters &params, cons
 }
 
 
+
+//Same as the above but do both the standard and symmetric V/W^dag momentum assignments. In this version we do not symmetrize the kaon momenta
+void doContractionsStandardAndSymmetricPion(const int conf, Parameters &params, const CommandLineArgs &cmdline, Lattice& lat,
+				      A2AvectorV<A2Apolicies> &V, A2AvectorW<A2Apolicies> &W,
+				      A2AvectorV<A2Apolicies> &V_s, A2AvectorW<A2Apolicies> &W_s,
+				      const typename A2Apolicies::SourcePolicies::MappingPolicy::ParamType &field3dparams){
+  //-------------------Fix gauge----------------------------
+  doGaugeFix(lat, cmdline.skip_gauge_fix, params);
+
+  //-------------------------Compute the kaon two-point function---------------------------------
+  StationaryKaonMomentaPolicy kaon_mom_std;
+  if(cmdline.do_kaon2pt) computeKaon2pt(V,W,V_s,W_s,kaon_mom_std,conf,lat,params,field3dparams,cmdline.randomize_mf);
+
+  //----------------------------Compute the sigma meson fields---------------------------------
+  StationarySigmaMomentaPolicy sigma_mom_std;
+  if(cmdline.do_sigma) computeSigmaMesonFields(V,W,sigma_mom_std,conf,lat,params,field3dparams);
+
+  //The pion two-point function and pipi/k->pipi all utilize the same meson fields. Generate those here
+  //For convenience pointers to the meson fields are collected into a single object that is passed to the compute methods
+  StandardPionMomentaPolicy pion_mom_std; //these are the W and V momentum combinations
+  MesonFieldMomentumContainer<A2Apolicies> mf_ll_con_std; //stores light-light meson fields, accessible by momentum
+  MesonFieldMomentumContainer<A2Apolicies> mf_ll_con_2s_std; //Gparity only
+
+  computeLLmesonFields(mf_ll_con_std, mf_ll_con_2s_std, V, W, pion_mom_std, conf, lat, params, field3dparams, cmdline.randomize_mf);
+
+  //----------------------------Compute the pion two-point function---------------------------------
+  if(cmdline.do_pion2pt) computePion2pt(mf_ll_con_std, pion_mom_std, conf, params);
+    
+  //------------------------------I=0 and I=2 PiPi two-point function---------------------------------
+  if(cmdline.do_pipi) computePiPi2pt(mf_ll_con_std, pion_mom_std, conf, params);
+
+  //--------------------------------------K->pipi contractions--------------------------------------------------------
+  StandardLSWWmomentaPolicy lsWW_mom_std;
+  LSWWmesonFields mf_ls_ww_con_std;
+  if(cmdline.do_ktopipi){
+    computeKtoPiPi(mf_ll_con_std,mf_ll_con_2s_std,V,W,V_s,W_s,lat,field3dparams,pion_mom_std,lsWW_mom_std,conf,params, cmdline.randomize_mf,&mf_ls_ww_con_std);
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  //Do the reverse momentum assignment, average with standard and repeat measurements
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  
+#ifdef DISTRIBUTED_MEMORY_STORAGE_REUSE_MEMORY
+  {
+    if(!UniqueID()){
+      std::ostringstream os; DistributedMemoryStorage::block_allocator().stats(os);
+      printf("Trimming block allocator. Current stats: %s\n",os.str().c_str()); fflush(stdout);
+    }
+    DistributedMemoryStorage::block_allocator().trim();
+    if(!UniqueID()){
+      std::ostringstream os; DistributedMemoryStorage::block_allocator().stats(os);
+      printf("Post-trim stats: %s\n",os.str().c_str()); fflush(stdout);
+    }
+  }
+#endif
+
+  ReversePionMomentaPolicy pion_mom_rev; //these are the W and V momentum combinations
+  MesonFieldMomentumContainer<A2Apolicies> mf_ll_con_symm; //stores light-light meson fields, accessible by momentum
+  MesonFieldMomentumContainer<A2Apolicies> mf_ll_con_2s_symm; //Gparity only
+  computeLLmesonFields(mf_ll_con_symm, mf_ll_con_2s_symm, V, W, pion_mom_rev, conf, lat, params, field3dparams, cmdline.randomize_mf, "_rev");
+
+  mf_ll_con_symm.average(mf_ll_con_std);
+  mf_ll_con_2s_symm.average(mf_ll_con_2s_std);
+  mf_ll_con_std.free_mem();
+  mf_ll_con_2s_std.free_mem();
+
+  //----------------------------Compute the pion two-point function---------------------------------
+  if(cmdline.do_pion2pt) computePion2pt(mf_ll_con_symm, pion_mom_std, conf, params, "_symm");
+    
+  //------------------------------I=0 and I=2 PiPi two-point function---------------------------------
+  if(cmdline.do_pipi) computePiPi2pt(mf_ll_con_symm, pion_mom_std, conf, params, "_symm");
+
+  //--------------------------------------K->pipi contractions--------------------------------------------------------
+  if(cmdline.do_ktopipi){  
+    printMem("Memory after computing W*W meson fields");
+    computeKtoPiPiContractions(V,W,V_s,W_s,mf_ls_ww_con_std,mf_ll_con_symm,mf_ll_con_2s_symm,pion_mom_std,conf,params,"_symmpi");
+  }
+}
+
+
+
 void doContractions(const int conf, Parameters &params, const CommandLineArgs &cmdline, Lattice& lat,
 		    A2AvectorV<A2Apolicies> &V, A2AvectorW<A2Apolicies> &W,
 		    A2AvectorV<A2Apolicies> &V_s, A2AvectorW<A2Apolicies> &W_s,
@@ -152,6 +238,8 @@ void doContractions(const int conf, Parameters &params, const CommandLineArgs &c
 
 #ifdef USE_STANDARD_AND_SYMMETRIC_MOM_POLICIES
   doContractionsStandardAndSymmetric(conf,params,cmdline,lat,V,W,V_s,W_s,field3dparams);
+#elif defined(USE_STANDARD_AND_SYMMETRIC_PION_MOM_POLICIES)
+  doContractionsStandardAndSymmetricPion(conf,params,cmdline,lat,V,W,V_s,W_s,field3dparams);
 #else
   doContractionsBasic(conf,params,cmdline,lat,V,W,V_s,W_s,field3dparams);
 #endif
